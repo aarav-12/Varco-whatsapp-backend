d58c107030e2ea785a6bb94abb99c1a2c3f0dc7b
aaravmithrani <aaravmithrani@ailifebot.in>
2026-03-19 15:31:31 +0530

    Whatsapp should work now

--- services/whatsappService.js
+++ services/whatsappService.js
@@ -1,6 +1,39 @@
 /* eslint-disable no-undef */
-const sendWhatsAppMessage = async (phone, message) => {
-  console.log('[MOCK WHATSAPP]', phone, message);
+// const sendWhatsAppMessage = async (phone, message) => {
+//   console.log('[MOCK WHATSAPP]', phone, message);
+// };
+
+// module.exports = { sendWhatsAppMessage };
+const axios = require('axios');
+
+const sendWhatsAppMessage = async (to, message) => {
+  try {
+    const response = await axios.post(
+      `https://graph.facebook.com/v18.0/${process.env.PHONE_NUMBER_ID}/messages`,
+      {
+        messaging_product: "whatsapp",
+        to: to,
+        type: "text",
+        text: {
+          body: message
+        }
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`,
+          "Content-Type": "application/json"
+        }
+      }
+    );
+
+    console.log("[WHATSAPP API SUCCESS]", response.data);
+
+  } catch (error) {
+    console.error(
+      "[WHATSAPP API ERROR]",
+      error.response?.data || error.message
+    );
+  }
 };
 
 module.exports = { sendWhatsAppMessage };
